3c8922ead26cc04e7e41e464caacfb91ba4ed99f
Jihanchy <habibullahalquaderi2005@gmail.com>
2025-01-14 11:41:37 +0600

    initialized project and setup tailwind, daisyUi and shadcn

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <Button asChild><button className='bg-red-500'>Click me</button></Button>
+      
+    </>
+  )
+}
+
+export default App
